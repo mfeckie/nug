ca7546764c2959c3e8481e324b158b5036ccd409
Martin Feckie <mfeckie@gmail.com>
2020-05-12 14:40:14 +0800

    Update handler struct

--- lib/nug/handler.ex
+++ lib/nug/handler.ex
@@ -1,12 +1,14 @@
 defmodule Nug.Handler do
-  @enforce_keys [:matchers]
-  defstruct [:port, :socket, :ref, :matchers, :upstream_base_url]
+  @enforce_keys [:matchers, :upstream_base_url, :request_filename]
+  defstruct [:port, :socket, :ref, :matchers, :upstream_base_url, :listen_ip, :request_filename]
 
   @type t :: %__MODULE__{
     port: integer(),
     socket: port(),
     ref: reference(),
     matchers: list(%Nug.Matcher{}),
-    upstream_base_url: String.t()
+    upstream_base_url: String.t(),
+    listen_ip: tuple(),
+    request_filename: String.t()
   }
 end
