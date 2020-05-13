757e916f9a79b46353c45d97af498f16638e60c9
Martin Feckie <mfeckie@gmail.com>
2020-05-13 18:52:22 +0800

    Loosen dependencies

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nug.MixProject do
   def project do
     [
       app: :nug,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -24,14 +24,14 @@ defmodule Nug.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "1.4.0", only: [:dev, :test], runtime: false},
-      {:ex_doc, "0.22.0", only: :dev, runtime: false},
-      {:plug_cowboy, "2.2.1"},
-      {:cowlib, "2.8.0"},
-      {:jason, "1.2.1"},
-      {:tesla, "1.3.2"},
-      {:mint, "1.0.0"},
-      {:castore, "0.1.5"}
+      {:credo, "~> 1.4.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.22.0", only: :dev, runtime: false},
+      {:plug_cowboy, "~> 2.2.1", optional: true},
+      {:cowlib, "~> 2.8.0", optional: true},
+      {:jason, "~> 1.2.1", optional: true},
+      {:tesla, "~> 1.3.2", optional: true},
+      {:mint, "~> 1.0.0", optional: true},
+      {:castore, "~> 0.1.5", optional: true}
     ]
   end
 
