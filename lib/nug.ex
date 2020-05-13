6a08788791795c6b87c4fddb65c23ab6887866a3
Martin Feckie <mfeckie@gmail.com>
2020-05-13 17:29:17 +0800

    Add a with_proxy macro

--- lib/nug.ex
+++ lib/nug.ex
@@ -1,3 +1,25 @@
 defmodule Nug do
+  defmacro __using__(_opts) do
+    quote do
+      import Nug
+    end
+  end
 
+  defmacro with_proxy(upstream_url, recording_file, test_body) do
+    quote do
+      {:ok, pid} =
+        Nug.HandlerSupervisor.start_child(%Nug.Handler{
+          upstream_url: unquote(upstream_url),
+          recording_file: unquote(recording_file)
+        })
+
+      var!(address) = Nug.RequestHandler.listen_address(pid)
+
+      try do
+        unquote(test_body)
+      after
+        Nug.RequestHandler.close(pid)
+      end
+    end
+  end
 end
---
+++ test/setup_example_test.exs
@@ -0,0 +1,14 @@
+defmodule Nug.SetupExampleTest do
+  use ExUnit.Case, async: true
+  use Nug
+
+  test "With macro" do
+    with_proxy("https://duckduckgo.com", "test/fixtures/with-macro.json") do
+      client = TestClient.new("http://#{address}")
+
+      {:ok, response} = Tesla.get(client, "/", query: [q: "hello"])
+
+      assert response.status == 200
+    end
+  end
+end
