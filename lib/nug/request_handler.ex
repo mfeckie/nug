b3fc1b879b7d22e0b35b4ea2e48d95b162daddc1
Martin Feckie <mfeckie@gmail.com>
2020-05-13 09:51:38 +0800

    Add way of updating and saving recordings

--- lib/nug/request_handler.ex
+++ lib/nug/request_handler.ex
@@ -24,16 +24,26 @@ defmodule Nug.RequestHandler do
     GenServer.call(pid, :listen_address)
   end
 
+  def close(pid) do
+    GenServer.call(pid, :close)
+  end
+
+  def add_recording(%Tesla.Env{} = env, pid) do
+    GenServer.call(pid, {:add_recording, env})
+  end
+
   defp do_up(ref, port, handler) do
     {:ok, socket} = :ranch_tcp.listen(so_reuseport() ++ [ip: listen_ip(), port: port])
 
     handler =
       struct(handler, %{
+        pid: self(),
         port: port,
         ref: make_ref(),
         socket: socket,
         listen_ip: listen_ip()
       })
+      |> add_file()
 
     plug_options = [handler: handler, timeout: 10_000]
 
@@ -46,6 +56,20 @@ defmodule Nug.RequestHandler do
     {:ok, _pid} = Plug.Cowboy.http(Nug.RequestServer, plug_options, cowboy_options)
 
     handler
+  end
+
+  def add_file(%Nug.Handler{} = handler) do
+    case File.read(handler.recording_file) do
+      {:ok, file} ->
+        recordings =
+          Jason.decode!(file, keys: :atoms)
+          |> Enum.map(&struct(Nug.Recording, &1))
+
+        Map.put(handler, :recordings, recordings)
+
+      _ ->
+        handler
+    end
   end
 
   defp get_port(port \\ 0) do
@@ -81,5 +105,23 @@ defmodule Nug.RequestHandler do
     string_ip = Tuple.to_list(ip) |> Enum.join(".")
     listen_address = string_ip <> ":#{port}"
     {:reply, listen_address, state}
+  end
+
+  def handle_call({:add_recording, env}, _from, state) do
+    new_state =
+      Map.update(state, :recordings, [], fn recordings ->
+        [%Nug.Recording{response: env} | recordings]
+      end)
+
+    Process.put(:updated_recordings, true)
+    {:reply, :ok, new_state}
+  end
+
+  def handle_call(:close, _from, state) do
+    if Process.get(:updated_recordings) do
+      Nug.Recording.save(state)
+    end
+
+    {:stop, :normal, :ok, []}
   end
 end
