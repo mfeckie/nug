f0ea0d82ad2ce2143930dfacb6b493b7e6c61686
Martin Feckie <mfeckie@gmail.com>
2020-05-13 18:03:20 +0800

    Add docs for with_proxy

--- lib/nug.ex
+++ lib/nug.ex
@@ -1,10 +1,39 @@
 defmodule Nug do
+  @moduledoc """
+  Provides a macro for using Nug in tests
+  """
   defmacro __using__(_opts) do
     quote do
       import Nug
     end
   end
 
+  @doc ~S"""
+  with_proxy is convenience macro that will handle setup and teardown of your proxy server.
+
+  # Usage
+
+  The `with_proxy` macro starts the proxy server and provides you with a variable in the block called `address`
+  this is the server address that will be listening for requests made in the block
+  ```
+  defmodule TestCase do
+    use ExUnit.Case, async: true
+    use Nug
+
+    test "get response from API" do
+      with_proxy("https://www.example.com", "test/fixtures/example.json") do
+        # address is a variable that is created by the macro
+        client = TestClient.new("http://#{address}")
+
+        {:ok, response} = Tesla.get(client, "/", query: [q: "hello"])
+
+        assert response.status == 200
+      end
+    end
+  end
+  ```
+
+  """
   defmacro with_proxy(upstream_url, recording_file, test_body) do
     quote do
       {:ok, pid} =
