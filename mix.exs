7fd6bf0cb6d15e1d9fc44ef0634c3858c69e2d5b
Martin Feckie <mfeckie@gmail.com>
2020-05-13 22:47:06 +0800

    Reduce specificity of plug_cowboy

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Nug.MixProject do
     [
       {:credo, "~> 1.4.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22.0", only: :dev, runtime: false},
-      {:plug_cowboy, "~> 2.2", optional: true},
+      {:plug_cowboy, "~> 2.0", optional: true},
       {:cowlib, "~> 2.0", optional: true},
       {:jason, "~> 1.2", optional: true},
       {:tesla, "~> 1.3", optional: true},
