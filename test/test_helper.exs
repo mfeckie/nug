81becd3b6777391d802ff515c878e0345e57085d
Martin Feckie <mfeckie@gmail.com>
2020-05-13 14:13:42 +0800

    Move Test client to test_helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,12 @@
+defmodule TestClient do
+  def new(url) do
+    middleware = [
+      {Tesla.Middleware.BaseUrl, url},
+      Tesla.Middleware.JSON
+    ]
+
+    Tesla.client(middleware, Tesla.Adapter.Mint)
+  end
+end
+
 ExUnit.start()
